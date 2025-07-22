1ce1acfc059e2eaff01d04d72b646cc4330dba58
VanajaMalla <68278874+VanajaMalla@users.noreply.github.com>
2025-07-22 05:56:05 +0530

    Added Number guessing game code

--- 02_Control_Flow/Number_Guessing_Game.py
+++ 02_Control_Flow/Number_Guessing_Game.py
@@ -1,3 +1,21 @@
 # Descrption: 
 
 # This Number guessing game project is based on python concepts : Control flow (if-else, loops)
+
+import random
+
+secret_number = random.randint(1,100)
+
+while True:
+    try:
+        guess = int(input("Enter your Guess:"))
+        if guess == secret_number:
+            print("🎉Congratulations! You won game")
+            break
+        elif guess > secret_number:
+            print("Your guess is greater than secret number ")
+        else:
+            print("Your guess is lesser than secret number ")
+    except ValueError:
+        print("Ouch! Please Enter valid Number.")
+    
